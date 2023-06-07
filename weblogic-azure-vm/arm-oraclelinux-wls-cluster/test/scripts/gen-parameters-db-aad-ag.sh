fc1f7e41092400a1af19976983beef6071c922a5
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-06-07 12:35:15 +0530

    Updated parameters schema version Signed-off-by: Sanjay Mantoor <sanjay.mantoor@oracle.com>

--- weblogic-azure-vm/arm-oraclelinux-wls-cluster/test/scripts/gen-parameters-db-aad-ag.sh
+++ weblogic-azure-vm/arm-oraclelinux-wls-cluster/test/scripts/gen-parameters-db-aad-ag.sh
@@ -10,7 +10,7 @@ read  parametersPath repoPath testbranchName keyVaultName keyVaultResourceGroup 
 
 cat <<EOF > ${parametersPath}
 {
-    "\$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "\$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
     "contentVersion": "1.0.0.0",
     "parameters": {
         "_artifactsLocation": {
