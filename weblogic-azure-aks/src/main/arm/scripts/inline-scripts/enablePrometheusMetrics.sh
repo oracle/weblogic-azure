7715ec026ad9316d402d8da3a56ea4ca0c053726
galiacheng <haixia.cheng@microsoft.com>
2024-04-01 12:49:15 +0800

    work around "module 'azure.core.pipeline.policies' has no attribute 'SensitiveHeaderCleanupPolicy'"

--- weblogic-azure-aks/src/main/arm/scripts/inline-scripts/enablePrometheusMetrics.sh
+++ weblogic-azure-aks/src/main/arm/scripts/inline-scripts/enablePrometheusMetrics.sh
@@ -224,12 +224,15 @@ function wait_for_keda_ready(){
 
 # https://learn.microsoft.com/en-us/azure/azure-monitor/containers/integrate-keda
 function enable_keda_addon() {
-    az extension add -n aks-preview
+    az extension remove --name k8s-extension
+    az extension add --name aks-preview
 
     local oidcEnabled=$(az aks show --resource-group $AKS_CLUSTER_RG_NAME --name $AKS_CLUSTER_NAME --query oidcIssuerProfile.enabled)
     local workloadIdentity=$(az aks show --resource-group $AKS_CLUSTER_RG_NAME --name $AKS_CLUSTER_NAME --query securityProfile.workloadIdentity)
 
     if [[ "${oidcEnabled,,}" == "false" || -z "${workloadIdentity}" ]]; then
+        # mitigate https://github.com/Azure/azure-cli/issues/28649
+        pip install --upgrade azure-core
         az aks update -g $AKS_CLUSTER_RG_NAME -n $AKS_CLUSTER_NAME --enable-workload-identity --enable-oidc-issuer
         utility_validate_status "Enable oidc and worload identity in AKS $AKS_CLUSTER_NAME."
     fi
