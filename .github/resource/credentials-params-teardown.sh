51ab4ae4521cce148077ced4c8a03564752a0b11
zhihaoguo <zhihaoguo@microsoft.com>
2025-03-10 20:09:23 +0800

    fix: change 'remove' to 'delete' for GitHub CLI secret teardown

--- .github/resource/credentials-params-teardown.sh
+++ .github/resource/credentials-params-teardown.sh
@@ -7,7 +7,7 @@ echo "teardown-credentials.sh - Start"
 yq eval -o=json '.[]' "$param_file" | jq -c '.' | while read -r line; do
     name=$(echo "$line" | jq -r '.name')
     value=$(echo "$line" | jq -r '.value')
-    gh secret --repo $(gh repo set-default --view) remove "$name"
+    gh secret --repo $(gh repo set-default --view) delete "$name"
 done
 
 echo "teardown-credentials.sh - Finish"
